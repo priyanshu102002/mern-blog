84ec45ee28008db8473a6d67de59441901bcb09e
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-29 09:40:13 +0530

    Add react-router-dom package and update App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Projects from "./pages/Projects";
+
 function App() {
-    return <>
-    <h1>Hello</h1></>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
