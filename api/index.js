410c19d3c3fc328a6fa24c9b4f4a147962c5a5fd
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-29 10:46:50 +0530

    Add dotenv and mongoose dependencies, configure express middleware, and update MongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB!!');
+}).catch((err) => {
+    console.log('Error connecting to MongoDB', err);
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!!');
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!!`);
 });
