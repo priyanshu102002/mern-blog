39c45b877da60836a313100ea0cc615e347c12aa
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-30 18:51:04 +0530

    redux persist added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
 import userReducer from "./user/userSlice"
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+// Combine all reducers
+const rootReducer = combineReducers({
+  user: userReducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1
+}
+
+// Using rootReducer, we can create presisted Reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  // prevent serializable error
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false
+  })
 })
+
+export const persistor = persistStore(store)
