f013208715b285d74f98e16293d1789949dfd74d
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-30 09:35:44 +0530

    signup ui completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/signup" element={<SignUp />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/projects" element={<Projects />} />
