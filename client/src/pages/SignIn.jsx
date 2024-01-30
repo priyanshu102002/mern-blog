1c58a07ac9021a24261877b3b3cd6595e4e4b2d3
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-30 11:39:57 +0530

    Refactor SignIn component to use flowbite-react components and add form validation

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import { Alert, Button, Label, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+    const [formData, setFormData] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const navigate = useNavigate();
 
-export default SignIn
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Check if all fields are filled
+        if (!formData.email || !formData.password) {
+            setErrorMessage("Please fill out all the fields.");
+            return;
+        }
+
+        try {
+            setLoading(true);
+            setErrorMessage(null); // Reset error message
+            const response = await fetch("/api/auth/signin", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await response.json();
+            if (data.success === false) {
+                setLoading(false);
+                setErrorMessage("This email or password is not correct.");
+                return;
+            }
+            setLoading(false);
+            if (response.ok) {
+                navigate("/");
+            }
+        } catch (error) {
+            setErrorMessage("Api is not working. Please try again later.");
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="min-h-screen mt-20">
+            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+                <div className="flex-1">
+                    <Link to="/" className="font-bold dark:text-white text-4xl">
+                        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                            Sahand's
+                        </span>
+                        Blog
+                    </Link>
+                    <p className="text-sm mt-5">
+                        This is a demo project. You can sign in with your email
+                        and password or with Google.
+                    </p>
+                </div>
+                <div className="flex-1">
+                    <form
+                        className="flex flex-col gap-4"
+                        onSubmit={handleSubmit}
+                    >
+                        <div>
+                            <Label value="Your email" />
+                            <TextInput
+                                type="email"
+                                placeholder="name@company.com"
+                                id="email"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <Label value="Your password" />
+                            <TextInput
+                                type="password"
+                                placeholder="Password"
+                                id="password"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <Button
+                            gradientDuoTone="purpleToPink"
+                            type="submit"
+                            disabled={loading}
+                        >
+                            {loading ? "Loading..." : "Sign Up"}
+                        </Button>
+                    </form>
+                    <div className="flex gap-2 text-sm mt-5">
+                        <span>Don't have an account?</span>
+                        <Link to="/sign-up" className="text-blue-500">
+                            Sign Up
+                        </Link>
+                    </div>
+                    {errorMessage && (
+                        <Alert color="failure" className="mt-5">
+                            {errorMessage}
+                        </Alert>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignIn;
