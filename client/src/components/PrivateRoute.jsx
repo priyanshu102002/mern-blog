4c2eb81917a9561f3afdff93d4e9ea46743f1172
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-31 11:40:18 +0530

    comments updated in private routes

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-// useNavigate is a hook that allows us to programmatically navigate to a different route
-// Navigate is a component 
-
 const PrivateRoute = () => {
     const { currentUser } = useSelector((state) => state.user);
     return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
 export default PrivateRoute;
+
+// useNavigate is a hook that allows us to programmatically navigate to a different route
+// Navigate is a component 
+// currentUser means that if the user is logged in, then they can access the private route
