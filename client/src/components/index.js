24f1a2198ff6df5930b700b69511ed27ca53ae84
priyanshukr449 <priyanshukr449@gmail.com>
2024-02-01 10:32:30 +0530

    sidebar functionallity added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as PrivateRoute } from "./PrivateRoute";
+export { default as DashSidebar } from "./DashSidebar";
+export { default as DashProfile } from "./DashProfile";
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { DashProfile, DashSidebar } from "../components";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+    const location = useLocation();
+    const [tab, setTab] = useState("");
 
-export default Dashboard
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get("tab");
+        if (tabFromUrl) setTab(tabFromUrl);
+    },[location]);
+
+    return <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      {tab === "profile" && <DashProfile />}
+    </div>;
+};
+
+export default Dashboard;
