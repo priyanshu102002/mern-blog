daf2604b1006effdc92f5316ed3d18a788d434a6
priyanshukr449 <priyanshukr449@gmail.com>
2024-02-03 21:17:11 +0530

    temp image uploading done

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,41 @@
 import { Button, TextInput } from "flowbite-react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function DashProfile() {
+    const filePickerRef = useRef(null);
     const { currentUser } = useSelector((state) => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImageFile(file);
+            const url = URL.createObjectURL(file);
+            setImageFileUrl(url);
+        }
+    };
+
+    // 4:23:30
+
     return (
         <div className="max-w-lg mx-auto p-3 w-full">
             <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
             <form className="flex flex-col gap-4">
-                <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    hidden
+                    ref={filePickerRef}
+                />
+                <div
+                    className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+                    onClick={() => filePickerRef.current.click()}
+                >
                     <img
-                        src={currentUser.photoPicture}
+                        src={imageFileUrl || currentUser.photoPicture}
                         alt="user"
                         className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
                     />
