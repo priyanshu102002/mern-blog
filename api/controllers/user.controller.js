2b176b242fd0b6a369f081757b7b3ab14dcd3d35
priyanshukr449 <priyanshukr449@gmail.com>
2024-02-07 10:09:20 +0530

    userUpdate route added in api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,56 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
+
 export const test = (req, res) => {
     res.send('Api Testing!');
+}
+
+export const updateUser = async (req, res, next) => {
+    // check the person is auth or not using cookie
+    if (req.params.userId !== req.user.id) {
+        return next(errorHandler(401, 'You are not authorized to perform this action'));
+    }
+    if (req.body.password) {
+        if (req.body.password < 6) {
+            return next(errorHandler(400, 'Password must be at least 6 characters'));
+        }
+        req.body.password = await bcryptjs.hash(req.body.password, 10);
+    }
+
+    if (req.body.username) {
+        if (req.body.username.length < 6 || req.body.username.length > 20) {
+            return next(errorHandler(400, 'Username length should be between 6 to 20 characters'));
+        }
+        if (req.body.username.includes(' ')) {
+            return next(errorHandler(400, 'Username should not contain any spaces'));
+        }
+        if (req.body.username !== req.body.username.toLowerCase()) {
+            return next(errorHandler(400, 'Username should be in lowercase'));
+        }
+        if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+            return next(errorHandler(400, 'Username should not contain any special characters'));
+        }
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                profilePicture: req.body.profilePicture,
+            }
+        }, { new: true });
+        const { password, ...rest } = updatedUser._doc;
+
+        res.status(200).json(rest);
+
+    } catch (error) {
+        return next(errorHandler(500, 'Internal Server Error'));
+    }
+
+
+
 }
--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 // CORS
 app.use(cors({
