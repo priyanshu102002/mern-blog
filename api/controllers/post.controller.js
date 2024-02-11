cafec27d4e51dd296861569b66cf929ba1e80bd5
priyanshukr449 <priyanshukr449@gmail.com>
2024-02-11 11:53:49 +0530

    create get post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,26 +1,78 @@
-import { errorHandler } from "../utils/error.js"
-import { Post } from "../models/post.model.js"
+import { errorHandler } from "../utils/error.js";
+import { Post } from "../models/post.model.js";
 
 export const create = async (req, res, next) => {
-
     if (!req.user.isAdmin) {
-        return next(errorHandler(403, 'You are not authorized to create a post'))
+        return next(
+            errorHandler(403, "You are not authorized to create a post")
+        );
     }
 
     if (!req.body.title || !req.body.content) {
-        return next(errorHandler(400, 'Title and Content are required'))
+        return next(errorHandler(400, "Title and Content are required"));
     }
 
-    const slug = req.body.title.toLowerCase().split(' ').join('-').replace(/[^a-zA-Z0-9-]/g, '');
+    const slug = req.body.title
+        .toLowerCase()
+        .split(" ")
+        .join("-")
+        .replace(/[^a-zA-Z0-9-]/g, "");
 
     const newPost = new Post({
-        ...req.body, slug, userId: req.user.id
-    })
+        ...req.body,
+        slug,
+        userId: req.user.id,
+    });
 
     try {
         const savedPost = await newPost.save();
-        res.status(201).json(savedPost)
+        res.status(201).json(savedPost);
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { categories: req.query.category }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
+            ...(req.query.searchTerm && {
+                $or: [
+                    { title: { $regex: req.query.searchTerm, $options: "i" } },
+                    {
+                        content: {
+                            $regex: req.query.searchTerm,
+                            $options: "i",
+                        },
+                    },
+                ],
+            }),
+        })
+            .sort({ updatedAt: sortDirection })
+            .skip(startIndex)
+            .limit(limit);
+
+        const totalPosts = await Post.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+
+        const lastMonthPosts = await Post.countDocuments({
+            updatedAt: { $gte: oneMonthAgo },
+        })
+
+        res.status(200).json({ posts, totalPosts, lastMonthPosts });
+    } catch (error) {
+        next(error);
+    }
+};
